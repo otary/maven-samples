5cb2625a8abd794b170b5cc9520bd4669cd2d6b6
chenzw <656469722@qq.com>
2019-07-29 18:42:20 +0800

    initialization

---
+++ maven-plugin-samples/maven-javadoc-plugin-samples/README.md
@@ -0,0 +1,4 @@
+# maven-javadoc-plugin-samples
+
+生成javadoc
+
---
+++ maven-plugin-samples/maven-javadoc-plugin-samples/src/main/java/cn/chenzw/maven/plugin/TestApp.java
@@ -0,0 +1,29 @@
+package cn.chenzw.maven.plugin;
+
+/**
+ * 测试APP
+ *
+ * @author chenzw
+ */
+public class TestApp {
+
+
+    /**
+     * 测试方法
+     *
+     * @param id ID
+     * @return 返回参数
+     */
+    public String test(String id) {
+        return "test";
+    }
+
+    /**
+     * hello
+     *
+     * @return 返回值
+     */
+    public String hello() {
+        return "hello";
+    }
+}
